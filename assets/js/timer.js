df4d405dfa46856e7c0c5c65083ad77b30733764
DanArmstrong124 <danielarmstrong124@gmail.com>
2021-04-08 22:07:25 +0000

    FIxed Timer

--- assets/js/timer.js
+++ assets/js/timer.js
@@ -4,7 +4,7 @@ var timedisplay = $(' #time ');
 
 setInterval(function(){
     var today = new Date();
-    var date = today.getDate()+'-'+(today.getMonth()+1)+'-'+today.getFullYear();
+    var date = today.getDate()+'/'+(today.getMonth()+1)+'/'+today.getFullYear();
     var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
     day.html(date);
     timedisplay.html(time);
