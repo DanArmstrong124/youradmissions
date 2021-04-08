5a0f2664496ec26af8dc3f1ebc6724982cf09edc
DanArmstrong124 <danielarmstrong124@gmail.com>
2021-04-08 22:04:55 +0000

    Fixes

--- assets/js/timer.js
+++ assets/js/timer.js
@@ -3,9 +3,9 @@ var timedisplay = $(' #time ');
 
 
 setInterval(function(){
-    var today = new Date().format('m-d-Y h:i:s');
-    //var date = today.getDate()+'-'+(today.getMonth()+1)+'-'+today.getFullYear()+;
+    var today = new Date();
+    var date = today.getDate()+'-'+(today.getMonth()+1)+'-'+today.getFullYear();
     var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-    day.html(today);
+    day.html(date);
     timedisplay.html(time);
 }, 100);
