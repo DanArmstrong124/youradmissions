2054a40c57515e6ce540193266c87af3cb8dcd39
DanArmstrong124 <danielarmstrong124@gmail.com>
2021-04-08 21:55:39 +0000

    test

--- assets/js/timer.js
+++ assets/js/timer.js
@@ -6,9 +6,6 @@ setInterval(function(){
     var today = new Date();
     var date = today.getDate()'-'+(today.getMonth()+1)+'-'+today.getFullYear()+;
     var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-}, 100);
-
-setInterval(function(){
     day.html(date);
     timedisplay.html(time);
-}, 1000);
+}, 100);
